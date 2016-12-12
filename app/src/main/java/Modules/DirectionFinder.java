0ce07e44d2c5968a3786dd879e66766f08c712a6
Atif Hussain <atifhussain324@gmail.com>
2016-12-12 17:39:06 -0500

    Menu bar added to Alerts.

--- app/src/main/java/Modules/DirectionFinder.java
+++ app/src/main/java/Modules/DirectionFinder.java
@@ -50,7 +50,7 @@ public class DirectionFinder {
     private String createUrl() throws UnsupportedEncodingException {
         String urlOrigin = URLEncoder.encode(origin, "utf-8");
         String urlDestination = URLEncoder.encode(destination, "utf-8");
-        return DIRECTION_URL_API + "origin=" + urlOrigin + "&destination=" + urlDestination + "&mode=transit&transit_mode=subway&transit_routing_preference=less_walking&key=" + GOOGLE_API_KEY;
+        return DIRECTION_URL_API + "origin=" + urlOrigin + "&destination=" + urlDestination + "&mode=transit&transit_mode=subway&key=" + GOOGLE_API_KEY;
     }
 
     public class DownloadRawData extends AsyncTask<String, Void, String> {
